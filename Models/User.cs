7603e33f0ea3f88f44a83442104d505131a6aa42
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-02-05 21:51:23 -0300

    UserModel

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogEFCore.Models;
+
+[Table("[Users]")]
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Bio { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+}
